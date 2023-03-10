30ed78dba29fe5967248f2a08eeca879c4cbc501
TanvirXen <tanvirish123@gmail.com>
2023-03-11 04:47:45 +0600

    Test

--- pages/contact.js
+++ pages/contact.js
@@ -1,7 +1,7 @@
 import React from "react";
-import Started from "../components/Sections/LandingPage/started";
-import News from "../components/Sections/LandingPage/News/index.js";
-import Maps from "../components/Sections/Contact/map/index.js";
+// import Started from "../components/Sections/LandingPage/started";
+// import News from "../components/Sections/LandingPage/News/index.js";
+// import Maps from "../components/Sections/Contact/map/index.js";
 export default function Contact() {
   return (
     <>
--- pages/service.js
+++ pages/service.js
@@ -1,12 +1,12 @@
 import React from "react";
-import Trust from "../components/Sections/LandingPage/trust";
+// import Trust from "../components/Sections/LandingPage/trust";
 
-import HeroService from "../components/Sections/Service/HeroService";
-import ServiceBusiness from "../components/Sections/Service/ServiceBusiness";
-import FAQ from "../components/Sections/About/faq";
-import News from "../components/Sections/LandingPage/News/index.js";
-import Started from "../components/Sections/LandingPage/started/index";
-import Overview from "../components/Sections/Service/overview";
+// import HeroService from "../components/Sections/Service/HeroService";
+// import ServiceBusiness from "../components/Sections/Service/ServiceBusiness";
+// import FAQ from "../components/Sections/About/faq";
+// import News from "../components/Sections/LandingPage/News/index.js";
+// import Started from "../components/Sections/LandingPage/started/index";
+// import Overview from "../components/Sections/Service/overview";
 export default function Service() {
   return (
     <div>
