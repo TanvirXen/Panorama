90cf2cd76327ff3ae4752577a1928ad6722cd70a
TanvirXen <tanvirish123@gmail.com>
2023-03-12 03:41:18 +0600

    Test

--- components/Sections/About/team/teamCard.js
+++ components/Sections/About/team/teamCard.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Grid from "@mui/material/Grid";
-
+import Typography from "@mui/material/Typography";
 export default function TeamCard() {
   return (
     <Grid xs={12} sm={6} md={6} lg={4} xl={3}>
