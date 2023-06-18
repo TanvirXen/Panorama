102f44b7929b630431ab939fd86860705eddde90
TanvirXen <tanvirish123@gmail.com>
2023-06-18 09:57:29 +0600

    Test

--- components/Sections/About/team/teamData.js
+++ components/Sections/About/team/teamData.js
@@ -1,10 +1,10 @@
 const TeamData = [
-  { id: "01", name: "Aref Ali", email: "arefali@panopramaas.com", linkedin: "https://www.linkedin.com/in/arefali/", title: "Chief Executive Officer", img: "team/aref_ali.png" },
-  { id: "02", name: "Selima Allen Hossain", email: "allenselima@panopramaas.com", linkedin: "https://www.linkedin.com/in/allen-selima-96233543/", title: "Country Director", img: "/team/allen_selima.png" },
+  { id: "01", name: "Aref Ali", email: "arefali@panoramamas.com", linkedin: "https://www.linkedin.com/in/arefali/", title: "Chief Executive Officer", img: "team/aref_ali.png" },
+  { id: "02", name: "Selima Allen Hossain", email: "allenselima@panoramamas.com", linkedin: "https://www.linkedin.com/in/allen-selima-hossain-96233543/", title: "Country Director", img: "/team/allen_selima.png" },
   { id: "03", name: "Imon Chandra Banik", email: "imonbanik@panoramamas.com", linkedin: "https://www.linkedin.com/in/imon-chandra-banik-qcpa-cxa-mba-bb903553/", title: "Financial Consultant", img: "/team/imon.jpg" },
   { id: "05", name: "Abdullah Alif", email: "alif@panoramamas.com", linkedin: "https://www.linkedin.com/in/4lif/", title: "Accounting and Finance Associate", img: "/team/abdullah_alif.png" },
   { id: "04", name: "Julie King", email: "julieali@panoramamas.com", linkedin: "", title: "Manager, USA", img: "/team/julie_king.png" },
-  { id: "06", name: "Nazmus Sadat", email: "sadat@panopramaas.com", linkedin: "https://www.linkedin.com/in/nazmussadatbuet/", title: "Consultant", img: "/team/nazmus_sadat.png" },
+  { id: "06", name: "Nazmus Sadat", email: "sadat@panoramamas.com", linkedin: "https://www.linkedin.com/in/nazmussadatbuet/", title: "Consultant", img: "/team/nazmus_sadat.png" },
   { id: "07", name: "Tanvir Ishtiaq", email: "tanvir@panoramamas.com", linkedin: "https://www.linkedin.com/in/tanvir-ishtiaq/", title: "Software Developer", img: "/team/tanvir_ishtiaq.png" },
   { id: "8", name: "Rifat Ara Bonnhy", email: "bonnyh@panoramamas.com", linkedin: "https://www.linkedin.com/in/bonnhy/", title: "Marketing and Content Associate", img: "/team/rifat_ara_bonnyh.png" },
   { id: "9", name: "Mithun Das Kabbo", email: "mithun@panoramamas.com", linkedin: "https://www.linkedin.com/in/mithundaskabbo/", title: "Consultant", img: "/team/mithun.png" },
