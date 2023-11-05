3ea8449e80360c45464d0b454d8dbf79c52dcda5
Tanvir Ishtiaq <tanvirish123@gmail.com>
2023-11-05 20:19:43 +0600

    Create serviceType.js

---
+++ components/Sections/LandingPage/services/serviceType.js
@@ -0,0 +1,65 @@
+const ServiceType = [
+    {
+      name: "Business Assessment",
+      desc: "Unlock your business potential with our comprehensive assessment. Identify your short-comings, analyse their nature and formulate the best solutions to overcome your challenges with us.",
+      img: "/solution/assesmentBusiness.png",
+      type: "Business Assessment",
+      url: "/assessment",
+    },
+    {
+      name: "Fractional CFO",
+      desc: "Access high-level financial advice without the commitment of a full-time CFO through our Fractional CFO services. We provide expert guidance tailored to the specific needs of your business. With our flexible solution, you can benefit from the strategic insights and financial expertise of a seasoned CFO, optimizing your financial decision-making and driving sustainable growth. Our Fractional CFO services offer the perfect balance of expertise and cost-efficiency to support your business's financial success.",
+      img: "/solution/CFOfractional.png",
+      type: "Fractional CFO",
+      url: "/fractionalcfo",
+    },
+    {
+      name: "Business Consulting",
+      desc: "Panorama, a management consulting firm, offers a comprehensive range of services designed to enhance organizational performance and support the achievement of clients' goals. The firm collaborates closely with clients to identify areas requiring improvement, formulate effective strategies, implement solutions, and foster positive transformations. By leveraging their expertise, industry knowledge, and analytical abilities, Panorama empowers organizations to optimize their processes, overcome obstacles, and attain long-lasting growth.",
+      img: "/solution/businessConsulting.png",
+      type: "Business Consulting",
+      url: "/consulting",
+    },
+    {
+      name: "Research",
+      desc: "Harness the power of cutting-edge research services. Panorama offers innovative research services that enable organizations to tap into the power of valuable insights and make informed decisions. Our dedicated team provides actionable data, industry trends, and in-depth competitive analysis to fuel your organization's growth and drive strategic initiatives. By leveraging our research services, you can stay ahead of the competition, make data-driven decisions, and seize opportunities that propel your organization forward. Unlock the full potential of your business and gain a competitive edge with our exceptional research expertise.",
+      img: "/solution/research.png",
+      type: "Research",
+      url: "/rdd",
+    },
+    {
+      name: "Training",
+      desc: "Panorama's services empower organizations by driving their growth through trans-formative training programs. Our dynamic workshops and engaging training sessions cover a wide range of areas, from leadership development to specialized skill-building. Designed to enhance employee capabilities and foster personal and professional growth, our programs equip your team with the skills necessary to excel in their roles. By leveraging our training solutions, you can unleash the full potential of your workforce and propel your organization forward.",
+      img: "/solution/training.png",
+      type: "Training",
+      url: "/",
+    },
+    {
+      name: "Documentation & Development",
+      desc: "Streamline your processes and ensure clarity. Panorama's meticulous documentation services enable organizations to streamline their processes and achieve clarity. Our expertise lies in creating comprehensive and user-friendly documents that enhance efficiency. Whether it's developing policies and procedures or crafting workflow diagrams, our services ensure optimized operations and seamless workflow within your organization. With our meticulous attention to detail, we help establish clear guidelines that enhance productivity across your business.",
+      img: "/solution/docs.png",
+      type: "Documentation & Development",
+      url: "/rdd",
+    },
+    {
+      type: "IT Service",
+      url: "/digital_service",
+    },
+    {
+        name: "UI/UX Development",
+        desc: "Our team excels at UI/UX Development through a blend of creativity, expertise, and meticulous processes. We start by thoroughly understanding your goals and your target audience. We conduct in-depth research to grasp user behaviors and preferences. Then, we craft visually captivating User Interfaces (UI) that resonate with your brand identity. Our User Experience (UX) designs focus on seamless navigation, ensuring every interaction is intuitive and efficient.",
+        img: "/solution/code.webp",
+        type: "UI/UX Development",
+        url: "/digital_service",
+      },
+      {
+        name: "Website Development",
+        desc: "Our team designs and builds websites that not only look great but also function seamlessly, providing a strong online presence and enhancing user engagement.In website development we use MERN(MongoDB, Express,React,Node.js) Stack and AWS .",
+        img: "/solution/ui.webp",
+        type: "Website Development",
+        url: "/digital_service",
+      },
+  ];
+  
+  export default ServiceType;
+  
