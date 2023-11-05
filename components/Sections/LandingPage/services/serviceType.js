dc4f55d7b61ee1a079dc474337f514ea427202cb
Tanvir Laptop <tanvirish123@gmail.com>
2023-11-05 20:33:17 +0600

    Test

--- components/Sections/LandingPage/services/serviceType.js
+++ components/Sections/LandingPage/services/serviceType.js
@@ -3,16 +3,38 @@ const ServiceType = [
       name: "Business Assessment",
       desc: "Unlock your business potential with our comprehensive assessment. Identify your short-comings, analyse their nature and formulate the best solutions to overcome your challenges with us.",
       img: "/solution/assesmentBusiness.png",
-      type: "Business Assessment",
+      type: "Business Assessment App",
       url: "/assessment",
+    },
+    {
+      type: "Finance App",
+      url: "/digital_service",
+    },
+    {
+      type: "IT Service",
+      url: "/digital_service",
     },
     {
       name: "Fractional CFO",
       desc: "Access high-level financial advice without the commitment of a full-time CFO through our Fractional CFO services. We provide expert guidance tailored to the specific needs of your business. With our flexible solution, you can benefit from the strategic insights and financial expertise of a seasoned CFO, optimizing your financial decision-making and driving sustainable growth. Our Fractional CFO services offer the perfect balance of expertise and cost-efficiency to support your business's financial success.",
       img: "/solution/CFOfractional.png",
-      type: "Fractional CFO",
+      type: "Fractional CFO App",
       url: "/fractionalcfo",
     },
+    {
+        name: "UI/UX Development",
+        desc: "Our team excels at UI/UX Development through a blend of creativity, expertise, and meticulous processes. We start by thoroughly understanding your goals and your target audience. We conduct in-depth research to grasp user behaviors and preferences. Then, we craft visually captivating User Interfaces (UI) that resonate with your brand identity. Our User Experience (UX) designs focus on seamless navigation, ensuring every interaction is intuitive and efficient.",
+        img: "/solution/code.webp",
+        type: "UI/UX Development",
+        url: "/digital_service",
+      },
+      {
+        name: "Website Development",
+        desc: "Our team designs and builds websites that not only look great but also function seamlessly, providing a strong online presence and enhancing user engagement.In website development we use MERN(MongoDB, Express,React,Node.js) Stack and AWS .",
+        img: "/solution/ui.webp",
+        type: "Website Development",
+        url: "/digital_service",
+      },
     {
       name: "Business Consulting",
       desc: "Panorama, a management consulting firm, offers a comprehensive range of services designed to enhance organizational performance and support the achievement of clients' goals. The firm collaborates closely with clients to identify areas requiring improvement, formulate effective strategies, implement solutions, and foster positive transformations. By leveraging their expertise, industry knowledge, and analytical abilities, Panorama empowers organizations to optimize their processes, overcome obstacles, and attain long-lasting growth.",
@@ -41,24 +63,7 @@ const ServiceType = [
       type: "Documentation & Development",
       url: "/rdd",
     },
-    {
-      type: "IT Service",
-      url: "/digital_service",
-    },
-    {
-        name: "UI/UX Development",
-        desc: "Our team excels at UI/UX Development through a blend of creativity, expertise, and meticulous processes. We start by thoroughly understanding your goals and your target audience. We conduct in-depth research to grasp user behaviors and preferences. Then, we craft visually captivating User Interfaces (UI) that resonate with your brand identity. Our User Experience (UX) designs focus on seamless navigation, ensuring every interaction is intuitive and efficient.",
-        img: "/solution/code.webp",
-        type: "UI/UX Development",
-        url: "/digital_service",
-      },
-      {
-        name: "Website Development",
-        desc: "Our team designs and builds websites that not only look great but also function seamlessly, providing a strong online presence and enhancing user engagement.In website development we use MERN(MongoDB, Express,React,Node.js) Stack and AWS .",
-        img: "/solution/ui.webp",
-        type: "Website Development",
-        url: "/digital_service",
-      },
+
   ];
   
   export default ServiceType;
