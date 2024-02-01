9dc8bf0ceca4ff562337b2490b240279ee6cdb31
Tanvir Ishtiaq <tanvirish123@@gmail.com>
2024-02-01 17:42:14 +0600

    Test

---
+++ components/Sections/About/career/membership.js
@@ -0,0 +1,52 @@
+import React from "react";
+import Container from "@mui/material/Container";
+import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
+import { TextInput, Select, SelectItem, Checkbox, Button } from "@carbon/react";
+import CareerIcon from "../../../reUse/career";
+
+export default function Memberships() {
+  return (
+    <div className="abtprofiles">
+      <div className="anb">
+        <Container maxWidth="xl">
+          <Grid container spacing={0}>
+            <Grid sm={12} md={6} lg={5}>
+              <div className="crrs">
+                <CareerIcon />
+                <div className="btrn">
+                  <Typography variant="large" color="neutral.b900">
+                    Please contact us if you have questions about our hiring
+                    process.
+                  </Typography>
+                </div>
+              </div>
+            </Grid>
+            <Grid sm={12} md={6} lg={7}>
+              <div className="xcd">
+                <Typography variant="h4" color="neutral.b900">
+                  Careers
+                </Typography>
+                <br />
+                <br />
+              </div>
+              <div>
+                <div>
+                  <Typography variant="large" color="neutral.b900">
+                    Join our dynamic team at Panorama where passion meets
+                    purpose. Connect with us empower businesses and individuals
+                    to succeed, and elevate your career to new heights.
+                    <br />
+                    <br />
+                    Submit your resume and cover letter to <span style={{fontWeight:'400'}}> <a style={{color:'black'}} href="mailto:info@panoramamas.com">info@panoramamas.com</a> </span>
+                    today.
+                  </Typography>
+                </div>
+              </div>
+            </Grid>
+          </Grid>
+        </Container>
+      </div>
+    </div>
+  );
+}
